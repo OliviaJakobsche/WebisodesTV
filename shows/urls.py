13ab2406300c3f1b7d9958d84dc1ba8b16f65d43
Olivia Jakobsche <liqhta@gmail.com>
2018-03-12 00:05:22 +0100

    Added Show table (model) to admin site. Added url pattern for shows detail view. Added test show detail view.

--- shows/urls.py
+++ shows/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+    # /shows/
     url(r'^$', views.index, name='index'),
+
+    # /shows/1/
+    url(r'^(?P<show_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- shows/views.py
+++ shows/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from .models import Show
+
 
 def index(request):
-    return HttpResponse("Testowy widok homepage dla shows")
+    all_shows = Show.objects.all()
+    html='<h1>Lista seriali</h1>'
+
+    for show in all_shows:
+        url = '/shows/' + str(show.id) + '/'
+        html += '<a href="' + url + '">' + show.title + '(' + show.release + ')</a><br>'
+    return HttpResponse(html)
+
+
+def detail(request, show_id):
+    return HttpResponse("<h2>Serial o id: " + str(show_id) + '</h2>')
+
+
